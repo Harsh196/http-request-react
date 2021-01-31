c8d8adda5cde2223c1da7818df1f72af36cd92a9
Harsh196 <35832212+Harsh196@users.noreply.github.com>
2021-01-31 01:32:04 -0500

    HTTP request using Axios

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
+axios.defaults.baseURL ='http://jsonplaceholder.typicode.com'; //This URL will be used application wide. 
+axios.defaults.headers.common['Authorization']='AUTH TOKEN'; //Common Authorization token for all request
+axios.defaults.headers.post['Content-Type']='application/json'; //default content type for all post request
+
+axios.interceptors.request.use(request=>{
+    console.log(request);
+    return request;
+}, error=>{
+    console.log(error);
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(response=>{
+    console.log(response);
+    return response;
+}, error=>{
+    console.log(error);
+    return Promise.reject(error);
+});
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
